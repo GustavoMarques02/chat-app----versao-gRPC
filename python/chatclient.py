eea7332941a2f535861059662cd89e2523497d6b
Gustavo Marques Reis <gustavo_marquesr@discente.ufg.br>
2022-07-28 08:20:22 -0300

    Update chatclient.py

--- python/chatclient.py
+++ python/chatclient.py
@@ -11,8 +11,8 @@ import chatserver_pb2_grpc
 def run():
     me = str(sys.argv[1]) # User's name (as registered in the registry. E.g., Alice, Bob, ...)
     while True:
-        dest = input("ENTER DESTINATION: ")
-        msg = input("ENTER MESSAGE: ")
+        dest = str(input("ENTER DESTINATION: "))
+        msg = str(input("ENTER MESSAGE: "))
         with grpc.insecure_channel(const.CHAT_SERVER_HOST+':'+const.CHAT_SERVER_PORT) as channel:
             stub = chatserver_pb2_grpc.chatserverStub(channel)
             stub.SendMessage(msg, dest, me)
